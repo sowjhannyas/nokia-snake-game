d055592785007e57d4622da7f9617e319f5d9e1f
comps420 <74553232+comps420@users.noreply.github.com>
2020-11-16 20:58:13 +0530

    Create snake.js

---
+++ snake.js
@@ -0,0 +1,55 @@
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = 1;
+  this.ySpeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.update = function() {
+  
+    if(this.total === this.tail.length){
+       for (var i = 0; i < this.tail.length - 1;   i++){
+      this.tail[i] = this.tail[i+1];
+    }
+    }
+    
+    this.tail [this.total-1] = createVector(this.x,this.y)
+    
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }
+
+  this.eat = function() {
+    if (this.x == food.x && this.y == food.y) {
+      this.total++;
+      return true;
+    }
+    return false;
+  }
+
+  this.show = function() {
+    for (var i = 0; i < this.tail.length;   i++){
+      fill(255,0,0);
+      strokeWeight(0.1);
+      stroke(0);
+      rect(this.tail[i].x, this.tail[i].y, 1, 1);
+    }
+    fill(255);
+    strokeWeight(0.1);
+    stroke(0);
+    rect(this.x, this.y, 1, 1);
+  }
+
+  this.setDir = function(x, y) {
+    this.xSpeed = x;
+    this.ySpeed = y;
+  }
+
+
+  this.endGame = function() {
+    if (this.x > w - 1 || this.x < 0 || this.y > h - 1 ||       this.y < 0) {
+      return true;
+    }
+  }
+}
